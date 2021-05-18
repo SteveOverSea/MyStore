d0e4069396671912d53278d7e517cbacbf9dcc57
Stefan Rausch <rausch.stefan@edumail.at>
2021-05-18 08:39:18 +0200

    Generate ProductList component and Producty Type

---
+++ src/app/models/Product.ts
@@ -0,0 +1,13 @@
+export class Product {
+    name: string;
+    description: string;
+    price: number;
+    imageURL: string;
+
+    constructor() {
+        this.name = "";
+        this.description = "";
+        this.price = 0;
+        this.imageURL = "";
+    }
+}
