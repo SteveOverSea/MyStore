242f672938ac63e4830057a2b829bc3d593f18d4
Stefan Rausch <rausch.stefan@edumail.at>
2021-05-21 09:37:35 +0200

    Add prototypical login/sign up form

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from "../services/cart.service";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-navigation',
@@ -8,11 +9,26 @@ import { CartService } from "../services/cart.service";
 })
 export class NavigationComponent implements OnInit {
   cartCount: number = 0;
+  users: any[] = [];
+  username: string = "";
+  password: string = "";
 
-  constructor(private cart: CartService) { }
+  constructor(private cart: CartService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.cart.count.subscribe(count => this.cartCount = count);
   }
 
+  toggleForm(e: Event): void {
+    const form = document.getElementById("login-form") as HTMLFormElement;
+    form.hidden = !form.hidden;
+  }
+
+  onSubmit(): void {
+    alert("username:" + this.username + ", password: " + this.password);
+    // check if user exists
+    // otherwise ask if new one should be created
+
+    // LOGIN form should be an own child component on the navigation
+  }
 }
